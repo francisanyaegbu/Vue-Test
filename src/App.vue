e341401da40702bbbc4fbdebbfb4f5c2b7e4295f
Francis <anyaegbufrancis34@gmail.com>
2025-08-01 11:08:05 +0100

    added tailwindcss and postcss configuration files

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,4 @@
-<script setup>
-import { ref } from 'vue';  
-
-
-const name = ref("Francis Anyaegbu");
-const Age = ref("I am 19 years old.")
-const UpdateNameAge = () => {
-  name.value = "Francis Ebubechukwu Anyaegbu";
-  Age.value = "I am 25 years old.";
-};
-</script>
-
-
-
 <template>
-  <p>
-    {{ name }}
-  </p>
-  <p>
-    {{ Age }}
-  </p>
-  <button @click="UpdateNameAge">Click Me(for age update)</button>
+    <h1>Vue Jobs</h1>
 </template>
 
