1475d3c684a760e5887c0a80ccae13fad27e7513
Francis <anyaegbufrancis34@gmail.com>
2025-09-01 10:37:06 +0100

    added search input field

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,20 +4,21 @@ import { PhFilmReel } from '@phosphor-icons/vue'
 
 <template>
     <nav 
-    class="flex text-white items-center gap-20 absolute left-0 right-0 mx-6 py-4 top-4 bg-transparent border-b-2 border-neutal-400">
+        class="flex text-white items-center gap-20 absolute left-0 right-0 mx-6 py-4 top-4 bg-transparent border-b-2 border-neutal-400">
 
-    <div class="flex text-3xl items-center gap-2 font-bold">
-        <PhFilmReel :size="35" />
-        <span>Movly</span>
-    </div>
+        <div class="flex text-3xl items-center gap-2 font-bold">
+            <PhFilmReel :size="35" />
+            <span>Movly</span>
+        </div>
 
-    <div>
-        <ul class="flex items-center gap-4 font-semibold">
-            <li>Movies</li>
-            <li>Favorite Movies</li>
-            <li>Recently Watched</li>
-        </ul>
-    </div>
+        <div class="flex justify-between items-center w-full">
+            <ul class="flex items-center gap-4 font-semibold">
+                <li>Movies</li>
+                <li>Favorite Movies</li>
+            </ul>
+            <input type="search" name="search-movie" id="search-movie" placeholder="Search for a movie..."
+            class="bg-transparent border-2 border-neutral-400 rounded-md px-5 py-1 focus:outline-none focus:border-white"/>
+        </div>
         
-</nav>
+    </nav>
 </template>
